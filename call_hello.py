40f98d1c6031d43110b95275435d47f1e0f2848b
Yuval Prag <pugibela@gmail.com>
2015-03-15 22:33:10 -0700

    rev 5.11
    different style of the BC server did not work.

--- call_hello.py
+++ call_hello.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
 import sys
 import Pyro4
+import Pyro4.util
 from tranceiver import TransCiver
+sys.excepthook = Pyro4.util.excepthook
+#transciver = TransCiver()
+print("creating hook to te server")
 transciver = Pyro4.Proxy("PYRONAME:Yuvals.transciver")
+print("calling the Hello function")
+print("")
 transciver.Hello
+print("calling the get function")
+print("")
 transciver.Get
